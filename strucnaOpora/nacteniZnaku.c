f1aa8f7f4aa3ce01c542e3bf7aad90004c55d71a
pepehol <josef.holisu@gmail.com>
2022-01-20 22:03:57 +0100

    Uprava .gitignore

--- strucnaOpora/nacteniZnaku.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char x = getchar();
-    printf("Zadaný znak: %c\n", x);
-
-    return 0;
-}
--- strucnaOpora/tga_draw/utils.c
+++
@@ -1,7 +0,0 @@
-#include "utils.h"
-
-void utils_out_of_memory(const char *filename, const int lineno)
-{
-    fprintf( stderr, "Oops! Out of memory (%s:%i)\n", filename, lineno );
-    exit( -1 );
-}
